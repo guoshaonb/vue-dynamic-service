f815f25dccb9a50b076a66a76fd27b0b5db0bace
郭步 <2601972538@qq.com>
2022-04-26 16:38:41 +0800

    代码修改

--- config/config.js
+++ config/config.js
@@ -14,16 +14,21 @@ function getIPAdress() {
 }
 const myHost = getIPAdress();
 const localConfig = {
-    basename:'admin-common',
-    username:'root',
-    password:'123456',
-    host:'localhost'
-}
-const serverConfig = {
-    basename : 'admin-common',
-    username : 'guoshao_com',
-    password : '123456',
-    host : 'localhost'
+    basename: 'admin-common',
+    username: 'root',
+    password: '123456',
+    host: 'localhost'
 }
 
-module.exports = myHost === '10.0.8.11' ? serverConfig : localConfig
+//这里填写上你服务器上数据库的账号密码
+const serverConfig = {
+    basename: 'admin-common',
+    username: '', //这里部署上服务器的时候需要更改 -->数据库账号
+    password: '', //这里部署上服务器的时候需要更改 -->数据库密码
+    host: 'localhost' //如果是服务器本地数据库就是这个，否则请换成云数据库ip
+}
+
+//1、这里请在服务器上执行上面的getIPAdress()方法，
+//2、得到服务器ip后，修改serverIp为你服务器本地的ip
+const serverIp = '123456' //这里部署上服务器的时候需要更改 -->服务器本地ip
+module.exports = myHost === serverIp ? serverConfig : localConfig
